083dd03d91b4f62303bb8f2c27ee93c47c89d713
Alexander Shorin <Alexander Shorin@localhost>
2012-03-03 20:13:36 +0400

    Version bump.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except ImportError:
 
 setup(
     name = 'simpleubjson',
-    version = '0.2',
+    version = '0.3',
     description = 'Simple universal binary json decoder/encoder for Python.',
 
     author = 'Alexander Shorin',
