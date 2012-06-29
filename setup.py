615ad7de387a3140e1d64c8f685d010dc3f67a23
Alexander Shorin <Alexander Shorin@localhost>
2012-06-29 06:33:19 +0400

    Version bump to 0.4 release.
    Key changes:
    - Python 3.x support;
    - UBJSON Draft-9 support with Draft-8 at one time;
    - Small perfomance boost;
    - Break all things, that had directly relayed on decode/encode modules.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except ImportError:
 
 setup(
     name = 'simpleubjson',
-    version = '0.4-dev',
+    version = '0.4',
     description = 'Simple universal binary json decoder/encoder for Python.',
 
     author = 'Alexander Shorin',
