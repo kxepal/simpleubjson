e081502714249b66b15c11b6d737b717ef6411d6
Alexander Shorin <Alexander Shorin@localhost>
2012-07-08 07:43:37 +0400

    Refactor pprint function.
    - Inspect draft-8 and draft-9 within separate funcs;
    - Inspection of draft-9 is recursiveless;
    - Print [S] and [H] size as marker and actual size;
    - Some code cleanup.

--- simpleubjson/tools/inspect.py
+++ simpleubjson/tools/inspect.py
@@ -8,8 +8,9 @@
 #
 
 import sys
+from functools import partial
+import simpleubjson
 import simpleubjson.decoder as decoder
-
 
 def pprint(data, output=sys.stdout, allow_noop=True,
            indent='    ', max_level=None, spec='draft-8'):
@@ -36,50 +37,72 @@ def pprint(data, output=sys.stdout, allow_noop=True,
         if max_level is None or level <= max_level:
             output.write('%s' % (indent * level))
             output.write(data)
-    def inspect(stream, level, size):
-        for type, length, value in stream:
-            if length is None and value is None:
-                if spec == 'draft8':
-                    if type == 'E' and size == 255:
-                        level -= 1
-                    maybe_write('[%s]\n' % (type,), level)
-                    if type == 'E':
-                        return
-                elif spec == 'draft9':
-                    if type in 'AO':
-                        maybe_write('[%s]\n' % (type,), level)
-                        return inspect(stream, level + 1, 255)
-                    elif type == 'E':
-                        level -= 1
-                        maybe_write('[%s]\n' % (type,), level)
-                    else:
-                        maybe_write('[%s]\n' % (type,), level)
-            elif length is not None and value is None:
-                maybe_write('[%s] [%s]\n' % (type, length), level)
-                if type in 'oO':
-                    length = length == 255 and length or length * 2
-                    inspect(stream, level + 1, length)
-                else:
-                    inspect(stream, level + 1, length)
-            elif length is None and value is not None:
-                value = decode_tlv(None, type, length, value)
-                maybe_write('[%s] [%s]\n' % (type, value), level)
-            else:
-                value = decode_tlv(None, type, length, value)
-                maybe_write('[%s] [%s] [%s]\n' % (type, length, value), level)
-            if size != 255:
-                size -= 1
-                if not size:
+            output.flush()
+
+    def inspect_draft8(stream, level, container_size):
+        for marker, size, value in stream:
+            # standalone markers
+            if size is None and value is None:
+                maybe_write('[%s]\n' % (marker,), level)
+                if marker == 'E':
                     return
+
+            # sized containers
+            elif size is not None and value is None:
+                maybe_write('[%s] [%s]\n' % (marker, size), level)
+                if marker in 'oO':
+                    size = size == 255 and size or size * 2
+                inspect_draft8(stream, level + 1, size)
+
+            # plane values
+            elif size is None and value is not None:
+                value = decode_tlv(None, marker, size, value)
+                maybe_write('[%s] [%s]\n' % (marker, value), level)
+
+            # sized values
+            else:
+                value = decode_tlv(None, marker, size, value)
+                maybe_write('[%s] [%s] [%s]\n' % (marker, size, value), level)
+
+            if container_size != 255:
+                container_size -= 1
+                if not container_size:
+                    return
+
+    def inspect_draft9(stream, level, *args):
+        for marker, size, value in stream:
+            # standalone markers
+            if size is None and value is None:
+                if marker == 'E':
+                    level -= 1
+                maybe_write('[%s]\n' % (marker,), level)
+                if marker in 'AO':
+                    level += 1
+
+            # plane values
+            elif size is None and value is not None:
+                value = decode_tlv(None, marker, size, value)
+                maybe_write('[%s] [%s]\n' % (marker, value), level)
+
+            # sized values
+            else:
+                value = decode_tlv(None, marker, size, value)
+                pattern = '[%s] [%s] [%s] [%s]\n'
+                # very dirty hack to show size as marker and value
+                _stream = streamify(simpleubjson.encode(size, spec=spec))
+                for smarker, _, size in _stream:
+                    args = (marker, smarker, size, value)
+                maybe_write(pattern % args, level)
+
     if spec.lower() in ['draft8', 'draft-8']:
-        spec = 'draft8'
-        stream = decoder.streamify(data, decoder.MARKERS_DRAFT_8, allow_noop=allow_noop)
+        streamify = partial(decoder.streamify, markers=decoder.MARKERS_DRAFT_8)
         decode_tlv = decoder.decode_tlv_draft_8
+        inspect = inspect_draft8
     elif spec.lower() in ['draft9', 'draft-9']:
-        spec = 'draft9'
-        stream = decoder.streamify(data, decoder.MARKERS_DRAFT_9, allow_noop=allow_noop)
+        streamify = partial(decoder.streamify, markers=decoder.MARKERS_DRAFT_9)
         decode_tlv = decoder.decode_tlv_draft_9
+        inspect = inspect_draft9
     else:
         raise ValueError('Unknown or unsupported specification %s' % spec)
-    inspect(stream, 0, 255)
-    output.flush()
+
+    inspect(streamify(data, allow_noop=allow_noop), 0, 255)
