ee8f51984636500b4b052438d031ce2acb47c23a
Alexander Shorin <kxepal@gmail.com>
2013-07-13 15:05:16 +0400

    Fix install on Python 2.x.
    Wonder how I miss this...

--- setup.py
+++ setup.py
@@ -39,12 +39,12 @@ except ImportError:
 mod = imp.load_module('version',
                       *imp.find_module('version', ['./simpleubjson/']))
 
-long_description = open('README.rst', 'rb').read().decode().strip()
+long_description = open('README.rst', 'rb').read().decode('utf-8').strip()
 long_description += '''
 Changes
 =======
 '''
-long_description += open('CHANGES.rst', 'rb').read().decode().strip()
+long_description += open('CHANGES.rst', 'rb').read().decode('utf-8').strip()
 
 setup(
     name = 'simpleubjson',
